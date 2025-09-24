b3230f0a98ef4e50a06e2def27d53327ec6ccb7c
Devni <ar112741@fhss.sjp.ac.lk>
2025-09-24 21:43:58 +0530

    Added repo name as the basename in vite.config and App.jsx

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  // The 'base' option is crucial for GitHub Pages deployment.
+  // It should be the same as your repository name.
+  base: '/ict2233-ca-02-Devni-Hewasundara/',
   plugins: [react()],
-})
+});
