ab622854c527ac22f9de53a3c4b4a060491f04b3
Devni <ar112741@fhss.sjp.ac.lk>
2025-09-24 14:49:45 +0530

    Updated codes in Footer.jsx and added react-router-dom and saas packages

--- my-portfolio/src/components/Footer/Footer.jsx
+++ my-portfolio/src/components/Footer/Footer.jsx
@@ -1,56 +1,50 @@
-import { FiGithub, FiLinkedin, FiMail, FiHeart } from 'react-icons/fi'; //import  social mediaicons from react library
-import './Footer.scss';  //import Footer.scss for styling 
+import { FiGithub, FiLinkedin, FiMail, FiHeart } from 'react-icons/fi';
+import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      
-      <div className="footer_content">  {/*Apply styles for whole footer content*/}
-        <div className="footer_info">
-          
-          
-          <div className="social_media">
-            {/* Navigate to my GitHub profile */}
-            <a
-              href="https://github.com/Devni-Hewasundara"
-              target="_blank"
-              className="footer_links"
+      <div className="container">
+        <div className="footer__content">
+          {/* Social Links */}
+          <div className="footer__social">
+            <a 
+              href="https://github.com/Devni-Hewasundara" 
+              target="_blank" 
+              
+              className="footer__social-link"
               
             >
               <FiGithub />
             </a>
-
-            {/* Navigate to my Linkedin profile*/}
-            <a
-              href="https://linkedin.com/in/DevniVihara"
-              target="_blank"
-              className="footer_links"
+            <a 
+              href="https://linkedin.com/in/DevniVihara" 
+              target="_blank" 
+              
+              className="footer__social-link"
               
             >
               <FiLinkedin />
             </a>
-
-            {/* My Email account */}
-            <a
+            <a 
               href="mailto:devnivihara52@gmail.com"
-              className="footer_links"
-              
+              className="footer__social-link"
+              aria-label="Email Contact"
             >
               <FiMail />
             </a>
           </div>
 
-          {/*Display a copyright message*/}
+          {/* Copyright */}
           <div className="footer__copyright">
             <p>
               Made with <FiHeart className="footer__heart" /> by Devni Vihara © {new Date().getFullYear()}
             </p>
           </div>
-
         </div>
       </div>
     </footer>
   );
 };
 
-export default Footer;  //Export the component to use in another pages and components
+export default Footer;
